ce24ed84fb2ed77f135bb78c571c8f96863cb410
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-03-16 15:18:07 +0200

    Return if paramter for t is null or undefined

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -7,10 +7,11 @@ const movie_get = async (req, res) => {
 
     if (typeof title === undefined || title == null) {
         res.status(404).json({Response: false, Error: 'Parameter t is required. Search with title.'});
-    } else {
-        const movieResp = await movieModel.getMovie(req.query.t);
-        res.json(movieResp);
+        return;
     }
+
+    const movieResp = await movieModel.getMovie(req.query.t);
+    res.json(movieResp);
 };
 
 module.exports = {
