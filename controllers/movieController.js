ba3c2b7eca5f2127a0b51f812670debccfc394d2
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-03-16 13:19:11 +0200

    Move backend logic to model

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,18 +1,8 @@
-const axios = require('axios')
-require('dotenv').config()
-
-console.log(process.env.API_KEY);
+const movieModel = require('../models/movieModel');
 
 const movie_list_get = async (req, res) => {
-    const baseURL =  `https://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${req.query.t}`;
-
-    axios
-        .get(baseURL)
-        .then(response => {
-            return res.json(response.data);
-        }).catch(error => {
-            return res.json(error);
-        });
+    const movieResp = await movieModel.getMovie(req.query.t);
+    res.json(movieResp);
 };
 
 module.exports = {
