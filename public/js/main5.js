c0b38a0c94af6becfa5b1d65007e17e6c81fa8b7
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-03-25 10:00:51 +0200

    Movie response is shown on website

--- public/js/main5.js
+++ public/js/main5.js
@@ -8,6 +8,11 @@ const loginForm = document.querySelector('#login-form');
 const addUserForm = document.querySelector('#add-user-form');
 const searchBookForm = document.querySelector('#search-book');
 const bookInfo = document.querySelector('#bookInfo');
+const searchMovieForm = document.querySelector('#search-movie');
+const movieInfo = document.querySelector('#movieInfo');
+const year = document.querySelector('#year');
+
+year.value = new Date().getFullYear();
 
 // login
 loginForm.addEventListener('submit', async (evt) => {
@@ -91,6 +96,24 @@ searchBookForm.addEventListener('submit', async (evt) => {
   const response = await fetch(url + '/getBook?isbn=' + searchBookForm.elements['isbn'].value, fetchOptions);
   const json = await response.json();
   bookInfo.innerHTML = JSON.stringify(json);
+  console.log('search book response', json);
+});
+
+// search movie form
+searchMovieForm.addEventListener('submit', async (evt) => {
+  evt.preventDefault();
+  const data = serializeJson(searchBookForm);
+  const fetchOptions = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + sessionStorage.getItem('token'),
+    },
+  };
+  console.log('Token from storage: ', sessionStorage.getItem('token'));
+  const response = await fetch(url + '/getMovie?t=' + searchMovieForm.elements['t'].value + '&year=' + searchMovieForm.elements['year'].value + '&plot=' + searchMovieForm.elements['plot'].value, fetchOptions);
+  const json = await response.json();
+  movieInfo.innerHTML = JSON.stringify(json);
   console.log('search book response', json);
 });
 
